a2d54d02ba872ed25cb16db14e165bbe1358b24a
Rahul Padigela <rahulreddy@users.noreply.github.com>
2019-05-17 11:29:34 +0200

    improvement: update docker files

---
+++ tests/functional/aws-node-sdk/test/object/completeMultipartUpload.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+
+
+describe('Complete MPU - handling fuzzy metadata responses i.e 500', () => {
+
+    it('should compose the object when the op is retried - obj created in MD', () => {
+
+    });
+
+    it('should compose the object when the op is retried - obj not created in MD', () => {
+
+    });
+
+});
